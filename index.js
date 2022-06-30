8a1660e46147dcfc47086ffbde3f0835fb9a280d
Matthew Coplan <mcoplanwork@gmail.com>
2022-06-29 21:44:03 -0400

    added mongodb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const { Client, Intents, Collection } = require('discord.js');
 require('dotenv').config();
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
+const mongoose = require('mongoose');
 
 client.commands = new Collection();
 client.events = new Collection();
@@ -10,5 +10,12 @@ client.events = new Collection();
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client)
 })
+
+mongoose.connect(process.env.MONGODB_SRV, {
+}).then( () => {
+    console.log("Connected to DB");
+}).catch((err) => {
+    console.log(err)
+});
 
 client.login(process.env.DISCORD_TOKEN);
