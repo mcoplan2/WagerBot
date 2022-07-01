20dc9e557042819dcfbaba5191bf3424af64b99b
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-01 08:16:11 -0400

    added comments with future of program

--- commands/balance.js
+++ commands/balance.js
@@ -1,4 +1,7 @@
 module.exports = {
+    //TODO: 8ball ? Ask bot question and bet against server as a group?
+    //TODO: Freeforall ? Create a bet with a question and reacting to emote YES/NO Places bet
+    //TODO Deathmatch ? 1v1 against someone
     name: "balance",
     permissions: [],
     description: "Check your balance",
