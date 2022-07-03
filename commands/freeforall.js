252d23510f78aba0305d6acd1a0d2283e1735e31
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-02 21:00:28 -0400

    freeforall is now working!

--- commands/freeforall.js
+++ commands/freeforall.js
@@ -1,12 +1,15 @@
 const profileModel = require('../models/profileSchema');
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
+var yes_users = new Set();
+var no_users = new Set();
+
 module.exports = {
     name: "free for all",
     aliases: ["ffa"],
     permissions: [],
     cooldown: 10,
-    description: "Free for all against everyone that reacts!",
+    description: "Free for all against everyone that enters!",
     async execute(messageCreate, interaction, args, cmd, client, profileData) {
         // grab the string of the entire command
         // {!ffa String here}
@@ -19,27 +22,19 @@ module.exports = {
         const row = new MessageActionRow()
 			.addComponents(
 				new MessageButton()
-					.setCustomId('primary')
+					.setCustomId('yes')
 					.setLabel('Yes')
 					.setStyle('SUCCESS'),    
 			)
 			.addComponents(
 				new MessageButton()
-					.setCustomId('primary2')
+					.setCustomId('no')
 					.setLabel('No')
                     .setStyle('DANGER')
             );
     
-            // Grab any mentions from the user !FFA @User win their game
-            // ON !REGISTER CHANGE ROLE
-            // @ EVERYBODY WITH !REGISTER ROLE WHEN THIS COMMAND IS EXECUTED
-            // STORE INFORMATION IN BUTTONS
-            // in 15-20M @ EVERYBODY WHO ENTERED ASKING "DID +String ?" 
-            // IF CLICK YOU GUESSED SAME AS OUTCAME(i.e USER WON AND YOU CLICKED YES)
-                    // THEN YOU GAIN A NUMBER OF TOKENS IN UR WALLET
-            // ELSE YOU LOSE A NUMBER OF TOKENS IN YOUR WALLET
         newstring = "Will "+string;
-            // TODO MAKE A NEW EVENT WATCH VIDEO!
+        newstring2 = "Did "+string;
 
                 
         const newEmbed = new MessageEmbed()
@@ -47,48 +42,133 @@ module.exports = {
             .setTitle("Choose an Outcome")
             .setDescription(newstring)
             .setTimestamp()
-            .setThumbnail('attachment://index.jpg')
-        
-        const messageEmbed = await messageCreate.channel.send({embeds: [newEmbed], components: [row]})
 
-        const collector = messageEmbed.createMessageComponentCollector({ componentType: 'BUTTON', time: 6000})
+        const newEmbed2 = new MessageEmbed()
+            .setColor(0x00FFFF)
+            .setTitle("Choose the result")
+            .setDescription(newstring2)
+            .setTimestamp()
+        
+        let messageEmbed = await messageCreate.channel.send({embeds: [newEmbed], components: [row]})
+
+
+        const filter = i => ((i.customId === "yes") || (i.customId === "no"))
+        const collector = messageEmbed.createMessageComponentCollector({ filter, time: 12000})
         collector.on("collect", async (i) => {
             await i.reply(`${i.user.username} clicked on the ${i.customId} button.`);
         })
+
+        // Add each user to a Set based on which button they pressed.
         collector.on("end", async (collected) => {
-            console.log(`Collected ${collected.size} items`);
-            console.log(collected);
+            collected.forEach( async (value) => {
+                if(value.customId === "yes") {
+                    yes_users.add(value.user.id);
+                }
+                else if(value.customId === "no") {
+                    no_users.add(value.user.id);
+                }
+            })
+
+            // function to remove duplicates from each set incase they tried to enter both a 'Yes' and 'No' response
+            // NO CHEATERS!!
+            function getDifference(setA, setB) {
+                return new Set(
+                    [...setA].filter(element => !setB.has(element))
+                );
+            }
+            const set1 = yes_users;
+            const set2 = no_users;
+
+            const yes_users_no_dups = getDifference(set1, set2);
+            const no_users_no_dups = getDifference(set2, set1);
+            console.log(yes_users_no_dups);
+            console.log(no_users_no_dups);
+
+            // Create Another Embed after the above ends to pass in the result of if they won or lost.
+            let messageEmbed2 = await messageCreate.channel.send({embeds: [newEmbed2], components: [row]})
+            const filter = i => ((i.customId === "yes") || (i.customId === "no"))
+            const collector2 = messageEmbed2.createMessageComponentCollector({ filter, time: 6000})
+            collector2.on("collect", async (i) => {
+                row.components[0].setDisabled(true);
+                row.components[1].setDisabled(true);
+                messageEmbed2.edit({embeds: [newEmbed2], components: [row]});
+                await i.reply(`${i.user.username} clicked on the ${i.customId} button.`);
+            })
+            
+            // THIS WILL BE THE LOGIC FOR THE DATABASE
+            collector2.on("end", async (collected2) => {
+                collected2.forEach( async (value) => {
+                    if(value.customId === 'yes') {
+                        try {
+                        yes_users_no_dups.forEach( async (user) => {
+                            // SOME LOGIC HERE ABOUT AMOUNT OF TOKENS
+                            let all_entries = yes_users_no_dups.size + no_users_no_dups.size;
+                            let total_amount = 100 * all_entries;
+                            let size = yes_users_no_dups.size;
+                            let realamount = parseInt(total_amount / size);
+                            if (realamount == 100) realamount = 0;
+                            await profileModel.findOneAndUpdate({
+                                userID: user,
+                            }, {
+                                $inc: {
+                                    tokens: realamount
+                                },
+                            });
+                            messageCreate.send
+                        })
+
+                        no_users_no_dups.forEach( async (user) => {
+                            // If they lose, just subtract 100 from their total
+                            let amount = 100;
+                            await profileModel.findOneAndUpdate({
+                                userID: user,
+                            }, {
+                                $inc: {
+                                    tokens: -amount
+                                },
+                            });
+                        })
+                        } catch(err) {
+                            console.log(err);
+                        }
+                    }
+                    else if(value.customId === "no") {
+                        try {
+                        no_users_no_dups.forEach( async (user) => {
+                            // SOME LOGIC HERE ABOUT AMOUNT OF TOKENS
+                            let all_entries = yes_users_no_dups.size + no_users_no_dups.size;
+                            let total_amount = 100 * all_entries;
+                            let size = no_users_no_dups.size;
+                            let realamount = parseInt(total_amount / size);
+                            // this means
+                            if (realamount == 100) realamount = 0;
+                            await profileModel.findOneAndUpdate({
+                                userID: user,
+                            }, {
+                                $inc: {
+                                    tokens: realamount
+                                },
+                            });
+                        })
+
+                        yes_users_no_dups.forEach( async (user) => {
+                            // If they lose, just subtract 100 from their total
+                            let amount = 100;
+                            await profileModel.findOneAndUpdate({
+                                userID: user,
+                            }, {
+                                $inc: {
+                                    tokens: -amount
+                                },
+                            });
+                        })
+                    } catch (err) {
+                        console.log(err);
+                    }
+                    }
+                });
+                
         })
-
-        
-
-
-        /*
-        client.on('interactionCreate', interaction => {
-
-            if (!interaction.isButton()) return;
-
-            console.log(interaction.user.username);
-
-            const collector = interaction.message.createMessageComponentCollector({ componentType: 'BUTTON', time: 10 });
-
-            console.log(collector)
-
-            collector.on('collect', i => {
-	            if (i.user.id === interaction.user.id) {
-		            i.reply(`${i.user.id} clicked on the ${i.customId} button.`);
-	            } else {
-		            i.reply({ content: `These buttons aren't for you!`, ephemeral: true });
-	            }
-            });
-
-            collector.on('end', collected => {
-	            console.log(`Collected ${collected.size} interactions.`);
-            });
-
-        })
-
-        */
     }
-
+)}
 }
