44979b5772eddfd155d6bdf2f68d84ee6c4d8703
Matthew Coplan <mcoplanwork@gmail.com>
2022-07-03 20:05:13 -0400

    added bot status

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,9 @@
-module.exports = () => {
+require('dotenv').config();
+
+module.exports = async (client) => {
+
+    const prefix = process.env.PREFIX;
     console.log('Bot is ON!');
+
+    await client.user.setActivity(`for ${prefix}help`, { type: 'WATCHING' });
 }
