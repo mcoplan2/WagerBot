89683393712276dc9e25abbec7a34be31b57bfe5
Matthew Coplan <mcoplanwork@gmail.com>
2023-03-25 23:37:02 -0400

    changed 24/hour command

--- events/client/ready.js
+++ events/client/ready.js
@@ -6,10 +6,10 @@ module.exports = async (client) => {
     const prefix = process.env.PREFIX;
     console.log('Bot is ON!');
     cron.schedule('0 0 * * *', () => {
-        const channel = client.channels.cache.get('<channel-id>'); // replace <channel-id> with the actual channel ID
+        const channel = client.channels.cache.get('1084902149982531728'); // replace <channel-id> with the actual channel ID
         channel.send('?stop');
         setTimeout(() => {
-          channel.send('?listen');
+          channel.send('?');
         }, 30000); // wait 30 seconds before sending the second message
       });
 
